202c831783c5d5539abbd20abf42743f6b941837
rollen1 <nolovedivision@gmail.com>
2022-09-24 00:19:18 +0300

    clock to milliseconds

---
+++ 8kyu/clock-to-ms.js
@@ -0,0 +1,30 @@
+/* 
+
+Clock shows h hours, m minutes and s seconds after midnight.
+
+Your task is to write a function which returns the time since midnight in milliseconds.
+Example:
+
+h = 0
+m = 1
+s = 1
+
+result = 61000
+
+Input constraints:
+
+    0 <= h <= 23
+    0 <= m <= 59
+    0 <= s <= 59 
+    
+*/
+
+function past(h, m, s){
+  
+    let hoursMs = h * 3600000
+    let minMs = m * 60000
+    let secMs = s * 1000
+      
+    return hoursMs + minMs + secMs
+    
+    }
